77e6a13962f59e375b506189da96eb323506b353
MrinalGawade <118379111+MrinalGawade@users.noreply.github.com>
2022-11-17 11:13:36 +0530

    Add files via upload

---
+++ example/if_else1.java
@@ -0,0 +1,23 @@
+public class if_else1 {
+    public static void main(String[] args) {
+        int number = 600;
+        if(number < 400) {
+
+            System.out.println("Mrinal is beautiful");
+        }
+        else if(number < 300)
+        {
+            System.out.println("Mrinal is cat lover");
+
+        }
+        else if(number < 200)
+        {
+            System.out.println("Mrinal is magnetic");
+
+        }
+        else
+        {
+            System.out.println("number is greater than 400,300 and 200");
+        }
+    }
+}
